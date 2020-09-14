a3a899aad3f4c35afe8edb1267fc2eb33b5cd7a7
Leoesb33 <57182111+Leoesb33@users.noreply.github.com>
2020-09-14 17:25:22 +0200

    ajout js

---
+++ js-100/main.js
@@ -0,0 +1,24 @@
+const firstname = 'leo'
+const lastname = 'essomba'
+
+console.log(`Bonjour${firstname}${lastname}!`)
+
+function add(a,b){
+    return a + b
+}
+const result = add(4,6)
+console.log(result)
+
+
+
+function add(nom,prenom){
+    return nom + prenom
+}
+const famille = add(`leo`,`esb`)
+console.log(famille)
+
+function carré(nombre){
+    return nombre * nombre
+}
+const carré = add(6)
+console.log(carré)
